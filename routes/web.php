69107174a30e7c2dd9f828fd59a062443eac67f3
TEMPP1901 <temppol1901@gmail.com>
2024-10-18 11:21:31 +0700

    view-admin

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');  
+    return view('index');
 });
+
+
+Route::get('/admin', action: function () {
+    return view('admin.layout.master');
+});
+
+
+
+
